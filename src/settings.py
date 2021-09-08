63ff99fe5d6c60dee123852a052e805986b0b785
padhivikas <v.padhi@somaiya.edu>
2021-09-08 16:16:20 +0530

    added * in allowed host

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9i#gm=s_(w(v2kjotz41%dlnb-i)fe&@)!nn_rzh^kt07djh9w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
