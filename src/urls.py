fabfa17710f97517498dc9c1f03a7e9d44512848
padhivikas <v.padhi@somaiya.edu>
2021-09-08 15:59:35 +0530

    added .keep file

--- src/urls.py
+++ src/urls.py
@@ -18,10 +18,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-# urlpatterns = [
-#     path('', admin.site.urls),
-# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
     path('', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
