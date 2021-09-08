67e68294460cb756763a4a21477ef6e89d97143b
padhivikas <v.padhi@somaiya.edu>
2021-09-08 14:39:59 +0530

    added dj static

--- src/settings.py
+++ src/settings.py
@@ -77,13 +77,35 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'u978625445_startupcrm',
+#         'USER':'u978625445_startupcrm',
+#         'PASSWORD':'Pa55@startupcrm',
+#         'HOST':'162.159.24.201',
+#         'PORT':'3306',
+
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'startupcrm',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
